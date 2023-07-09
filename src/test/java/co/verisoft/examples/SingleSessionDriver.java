11783ae8fb09773a9533a0baeb85622ad4623781
Nir Gallner <nir@verisoft.ai>
2023-07-09 22:43:32 +0300

    Add single session driver class

---
+++ src/test/java/co/verisoft/examples/SingleSessionDriver.java
@@ -0,0 +1,48 @@
+package co.verisoft.examples;
+
+import co.verisoft.examples.pageobjects.WikipediaMainPage;
+import co.verisoft.examples.pageobjects.WikipediaResultPage;
+import co.verisoft.fw.asserts.Asserts;
+import co.verisoft.fw.selenium.drivers.VerisoftDriver;
+import co.verisoft.fw.selenium.drivers.factory.DriverCapabilities;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SingleSessionDriver extends BaseTest{
+
+    private WebDriver driver;
+
+    @DriverCapabilities
+    private DesiredCapabilities capabilities = new DesiredCapabilities();
+
+    {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+
+        capabilities.setBrowserName("chrome");
+        capabilities.setCapability("browserVersion", "113");
+        capabilities.setCapability("driverVersion", "113");
+        options.merge(capabilities);
+    }
+
+    public SingleSessionDriver(){
+        this.driver = new VerisoftDriver(capabilities);
+    }
+
+    @BeforeEach
+    public void setup(){
+        this.driver.get("https://www.wikipedia.org");
+    }
+
+    @Test
+    public void test1(){
+        WikipediaMainPage wikipediaMainPage = new WikipediaMainPage(driver);
+        WikipediaResultPage wikipediaResultPage = wikipediaMainPage.searchForTerm("Test Automation");
+        Asserts.assertTrue(wikipediaResultPage.getPageTitle().toLowerCase().contains("Test Automation".toLowerCase()),
+                "Should contain");
+    }
+}
