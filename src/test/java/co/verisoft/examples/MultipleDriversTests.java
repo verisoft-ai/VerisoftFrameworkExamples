8e454def16e95b6938bd496b63a8ac754fb569f0
Gili Eliach <gili.eliach@verisoft.co>
2024-05-19 11:57:39 +0300

    documentation

--- src/test/java/co/verisoft/examples/MultipleDriversTests.java
+++ src/test/java/co/verisoft/examples/MultipleDriversTests.java
@@ -11,7 +11,6 @@ import co.verisoft.fw.selenium.drivers.factory.DriverName;
 import co.verisoft.fw.selenium.drivers.factory.DriverUrl;
 import co.verisoft.fw.selenium.junit.extensions.DriverInjectionExtension;
 import co.verisoft.fw.selenium.junit.extensions.ScreenShotExtension;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
@@ -27,14 +26,25 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ContextConfiguration(classes = {CapabiitiesInjection.class})
 public class MultipleDriversTests {
 
+  /**
+   If no specific capabilities are defined, the driver will use this capabilities
+   */
     @DriverCapabilities
     EdgeOptions options = new EdgeOptions();
     {
         options.addArguments("--headless");
     }
+    /**
+     If no specific URL are defined, the driver will use this URL
+     */
     @DriverUrl
-    String url="http://95.216.217.133:4444/wd/hub/";
+    String url="http://1.2.3.4:4444/wd/hub/";
 
+    /**
+     *
+     * @param chromeDriver  initialized by bean-defined capabilities in CapabiitiesInjection.class, the url is the default- the field with the annotation @DriverUrl
+     * @param edgeDriver  initialized by bean-defined capabilities in CapabiitiesInjection.class, the url is the default- the field with the annotation @DriverUrl
+     */
     @Test
     public void twoDriversWithOnlyBeanCapabilities(
             @DriverCapabilities("chrome")  VerisoftDriver chromeDriver,
@@ -47,11 +57,11 @@ public class MultipleDriversTests {
         Asserts.assertEquals("Google", title, "We are in Google homepage in edge");
 
     }
-    @Test
-    public void clearCache()
-    {
-        WebDriverManager.chromedriver().clearDriverCache();    }
-
+    /**
+     *
+     * @param chromeDriver  initialized capabilities specified in a JSON configuration (my_capabilities.json). the url is the default- the field with the annotation @DriverUrl
+     * @param edgeDriver   initialized capabilities specified in a JSON configuration (my_capabilities.json), the url is the default- the field with the annotation @DriverUrl
+     */
     @Test
     public void twoDriversWithOnlyJsonCapabilities(
             @DriverCapabilities("chromeJson")  VerisoftDriver chromeDriver,
@@ -65,6 +75,11 @@ public class MultipleDriversTests {
 
     }
 
+    /**
+     *
+     * @param chromeDriver  initialized capabilities specified in a JSON configuration (my_capabilities.json). the url is the default- the field with the annotation @DriverUrl
+     * @param edgeDriver  initialized by bean-defined capabilities in CapabiitiesInjection.class, the url is the default- the field with the annotation @DriverUrl
+     */
     @Test
     public void twoDriversWithBeanAndJsonCapabilities(
             @DriverCapabilities("chromeJson")  VerisoftDriver chromeDriver,
@@ -78,6 +93,11 @@ public class MultipleDriversTests {
 
     }
 
+    /**
+     *
+     * @param chromeDriver  with driver name for  extracting it from VerisoftDriverManager
+     * @param edgeDriver  with driver name for extracting it from VerisoftDriverManager to edgedriver1
+     */
     @Test
     public void twoDriversWithOnlyOneCapabilitiesWithDriversNames(
            @DriverName("chrome bean") @DriverCapabilities("chrome")  VerisoftDriver chromeDriver,
@@ -92,6 +112,10 @@ public class MultipleDriversTests {
 
     }
 
+    /**
+     *
+     * @param chromeDriver initialized by bean-defined capabilities and URL in CapabiitiesInjection.class.
+     */
     @Test
     public void oneDriverWithCapabilitiesAndURLBean(
             @DriverCapabilities("chrome")  @DriverUrl("grid1") VerisoftDriver chromeDriver) {
@@ -100,14 +124,23 @@ public class MultipleDriversTests {
         Asserts.assertEquals("Google", title, "We are in Google homepage in chrome");
     }
 
+
+    /**
+     *
+     * @param chromeDriver initialized by bean-defined capabilities in CapabiitiesInjection.class. The url is hard-coded in the annotation
+     */
     @Test
     public void oneDriverWithCapabilitiesAndURLHardCoded(
-            @DriverCapabilities("chrome")  @DriverUrl("http://95.216.217.133:4444/wd/hub/") VerisoftDriver chromeDriver) {
+            @DriverCapabilities("chrome")  @DriverUrl("http://1.2.3.1:4444/wd/hub/") VerisoftDriver chromeDriver) {
         chromeDriver.get("http://www.google.com");
         String title = chromeDriver.getTitle();
         Asserts.assertEquals("Google", title, "We are in Google homepage in chrome");
     }
 
+    /**
+     *
+     * @param chromeDriver initialized by bean-defined capabilities in CapabiitiesInjection.class. the url is the default- the field with the annotation @DriverUrl
+     */
     @Test
     public void oneDriverWithCapabilitiesAndURLField(
             @DriverCapabilities("chrome") VerisoftDriver chromeDriver)
@@ -117,25 +150,36 @@ public class MultipleDriversTests {
         Asserts.assertEquals("Google", title, "We are in Google homepage in chrome");
     }
 
+    /**
+     *
+     * @param mobileDriver  initialized capabilities specified in a JSON configuration (my_capabilities.json). The url is hard-coded in the annotation
+     */
     @Test
     public void oneMobileDriverWithCapabilitiesAndURL(
-            @DriverCapabilities("mobileJson") @DriverUrl("http://127.0.0.1:4723/wd/hub/") VerisoftMobileDriver leumiDriver)
+            @DriverCapabilities("mobileJson") @DriverUrl("http://127.0.0.1:4723/wd/hub/") VerisoftMobileDriver mobileDriver)
     {
-        leumiDriver.findElement(By.id("SOME_ID")).click();
+        mobileDriver.findElement(By.id("SOME_ID")).click();
     }
-
+    /**
+     *
+     * @param mobileDriver  initialized capabilities specified in a JSON configuration (my_capabilities.json).
+     *                     The command executor is initialized by the proxy netty client factory like this:
+     *                     AppiumCommandExecutor(MobileCommand.commandRepository, new URL("<http://127.0.0.1:4723/wd/hub/>"), new ProxyNettyClient.Factory())
+     */
     @Test
     public void oneDriverWithCapabilitiesAndProxyCommandExecutor(
-            @DriverCapabilities("mobileJson") @DriverCommandExecutor("org.openqa.selenium.remote.http.netty.ProxyNettyClient$Factory") VerisoftMobileDriver chromeDriver)
+            @DriverCapabilities("mobileJson") @DriverCommandExecutor("org.openqa.selenium.remote.http.netty.ProxyNettyClient$Factory") VerisoftMobileDriver mobileDriver)
     {
-        chromeDriver.findElement(By.id("SOME_ID")).click();
+        mobileDriver.findElement(By.id("SOME_ID")).click();
     }
-
+    /**
+     * @param mobileDriver initialized by bean-defined capabilities and URL in CapabiitiesInjection.class.
+     */
     @Test
     public void oneDriverWithCapabilitiesAndProxyCommandExecutorBean(
-            @DriverCapabilities("mobileJson") @DriverCommandExecutor("commandExecutor") VerisoftMobileDriver chromeDriver)
+            @DriverCapabilities("mobileJson") @DriverCommandExecutor("commandExecutor") VerisoftMobileDriver mobileDriver)
     {
-        chromeDriver.findElement(By.id("SOME_ID")).click();
+        mobileDriver.findElement(By.id("SOME_ID")).click();
     }
 
 }
