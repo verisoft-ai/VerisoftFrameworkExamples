ab4dc2624677cf31534bcdf6546ab77a0de54b09
efrat_batya <efrat.greenberg@verisoft.co>
2024-09-18 16:04:17 +0300

    Add SoftAssertWithScreenshot example

--- src/test/java/co/verisoft/examples/SoftAssertWithScreenshotTest.java
+++ src/test/java/co/verisoft/examples/SoftAssertWithScreenshotTest.java
@@ -1,30 +1,27 @@
 package co.verisoft.examples;
 import co.verisoft.fw.CustomReportPortalExtension;
 import co.verisoft.fw.asserts.SoftAssertsScreenShot;
-import co.verisoft.fw.report.observer.Report;
 import co.verisoft.fw.selenium.drivers.VerisoftDriver;
 import co.verisoft.fw.selenium.drivers.factory.DriverCapabilities;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.io.File;
-
 @ExtendWith({CustomReportPortalExtension.class})
-public class SoftAssertWithScreenshotTest extends BaseTest {
-
+public class SoftAssertWithScreenshotTest extends BaseTest{
     @DriverCapabilities
     private ChromeOptions capabilities = new ChromeOptions();
     @Test
     public void AssertWithScreenshot(VerisoftDriver driver){
-        SoftAssertsScreenShot softAssertsScreenShot = new SoftAssertsScreenShot(driver);
-        driver.get("https://google.com/");
-        softAssertsScreenShot.assertTrue(false,"fail");
-        softAssertsScreenShot.assertTrue(true,"pass");
-        driver.get("https://bitbucket.org/nir_gallner/verisoftframeworkexamples/src/master/");
-        softAssertsScreenShot.assertTrue(false,"fail");
-        Report.info("Add file",new File("src/test/resources/screenush.png"));
-        softAssertsScreenShot.assertAll();
-    }
+            SoftAssertsScreenShot softAssertsScreenShot = new SoftAssertsScreenShot(driver);
 
+            boolean condition1 = false;
+            boolean condition2 = false;
+
+            driver.get("https://google.com/");
+            softAssertsScreenShot.assertTrue(condition1 ,"fail");
+            driver.get("https://translate.google.com/");
+            softAssertsScreenShot.assertTrue(condition2 ,"fail");
+            softAssertsScreenShot.assertAll();
+        }
 }
